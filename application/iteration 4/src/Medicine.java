b4651f705e225edf89ebfd920b5a6649aa86dcd8
NisaBeyza <91913304+NisaBeyza@users.noreply.github.com>
2021-12-19 16:09:32 +0100

    iteration-4

---
+++ application/iteration 4/src/Medicine.java
@@ -0,0 +1,62 @@
+
+public class Medicine {
+	private String medicineName;
+	private int barcode;
+	private int numberOfTabletsInside;
+	private int amountOfMedicineInside;
+	private String licenseOwner;
+	private int licenseNumber;
+	private int lotNumber;
+	private int expirationDate;
+	private String usage;
+	private int maximumUsageAmountPerDay;
+	private String id;
+	private String comment;
+	private String dosage;
+	private String period;
+	private String usageMethod;
+	private int usageNumber;
+	private int boxCount;
+	private String picture;
+
+	public void whatCanYouUseItFor() {
+		// TODO - implement Medicine.whatCanYouUseItFor
+		throw new UnsupportedOperationException();
+	}
+
+	public void whoCanNotUseThisMedicine() {
+		// TODO - implement Medicine.whoCanNotUseThisMedicine
+		throw new UnsupportedOperationException();
+	}
+
+	public void sideEffects() {
+		// TODO - implement Medicine.sideEffects
+		throw new UnsupportedOperationException();
+	}
+
+	public void getMedicine() {
+		// TODO - implement Medicine.getMedicine
+		throw new UnsupportedOperationException();
+	}
+
+	public void createMedicine() {
+		// TODO - implement Medicine.createMedicine
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAllMedicine() {
+		// TODO - implement Medicine.getAllMedicine
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateMedicine() {
+		// TODO - implement Medicine.updateMedicine
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteMedicine() {
+		// TODO - implement Medicine.deleteMedicine
+		throw new UnsupportedOperationException();
+	}
+
+}
